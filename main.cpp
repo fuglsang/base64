f5901d2a3fe9ffa34956a7c61d82f9fa848a6a5b
olgis <o.dmitricenko@gmail.com>
2017-04-16 18:02:26 +0200

    added sub_string var and released memory

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 int main(int argc, char** argv)
 {
 	const char* input_message = "here is a large haystack";
+	const char* sub_string = "is a";
 
 	char* encoded_message = (char*)malloc(base64_encoded_length(input_message));
 	base64_encode(encoded_message, input_message);
@@ -15,7 +16,10 @@ int main(int argc, char** argv)
 
 	printf("%s -> %s -> %s\n", input_message, encoded_message, decoded_message);
 
-	base64_find("is a", encoded_message);
+	base64_find(sub_string, encoded_message);
+
+	free(encoded_message);
+	free(decoded_message);
 
 	getchar();
 }
